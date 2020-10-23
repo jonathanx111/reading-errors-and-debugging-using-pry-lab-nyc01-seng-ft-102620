66eb666947cd90626d54107252b788c1b9ae70e6
Jonathan Xiao <jonathanx111@gmail.com>
2020-10-23 00:38:58 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,24 +1,16 @@
-# don't forget to add: require 'pry'
+ require 'pry'
 
-def get_user_input
-  gets.chomp
-end
+def two_sum(nums, target)
+  map = {}
 
-def prompt_user
-  puts "What would you like to do?"
-  puts "1.) Eat a hamburger."
-  puts "2.) Eat a ham."
-end
+  nums.each_with_index do |num, idx|
+    i = map[target - num]
+    return [i, idx] if i
 
-def selection(num)
-  if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+    map[num] = idx
+    binding.pry
   end
 end
 
-def runner
-  prompt_user
-  selection(get_user_input)
-end
+
+p two_sum([3,3], 6)
